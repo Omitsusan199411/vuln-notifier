1db17ca2e2d44be5dbd70a54b1f7d2013e2e90cd
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2026-05-14 02:50:48 +0900

    refactor: biome check

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
 	const [message, setMessage] = useState<string | null>(null);
@@ -11,5 +12,13 @@ export default function Home() {
 			.then((data) => setMessage(data.message));
 	}, []);
 
-	return <p>{message}</p>;
+	return (
+		<>
+			<p>{message}</p>
+			<Button className="border border-blue-500 text-blue-500 bg-white">
+				テストボタン1
+			</Button>
+			<Button variant="outline">テストボタン2</Button>
+		</>
+	);
 }
--- packages/web/vitest.setup.ts
+++ packages/web/vitest.setup.ts
@@ -5,6 +5,7 @@ import { server } from "@/tests/mocks/server";
 // testing-libraryのマッチャー等をvitestで使えるようにするため
 import "@testing-library/jest-dom/vitest";
 
+// mswサーバーのライフサイクルをグローバルで管理
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
