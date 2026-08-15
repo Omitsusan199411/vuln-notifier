182faa3550f4e6a6d0af2367c9e97d1c718e0be2
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2026-08-15 10:20:52 +0000

    test: add config vitest coverage

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -12,6 +12,12 @@ export default defineConfig({
 		coverage: {
 			provider: "v8",
 			reporter: ["text", "html"],
+			thresholds: {
+				lines: 60,
+				functions: 60,
+				branches: 60,
+				statements: 60,
+			},
 		},
 	},
 	server: {
