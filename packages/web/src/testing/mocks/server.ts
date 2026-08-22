a4d2a38280089b314f59beed08f4abdb6e6ccfb1
N.Mitsunaga <n.mitsunaga@churadata.okinawa>
2026-08-22 04:12:32 +0000

    fix: import path

--- packages/web/src/testing/mocks/server.ts
+++ packages/web/src/testing/mocks/server.ts
@@ -1,5 +1,5 @@
 import { setupServer } from "msw/node";
-import { appHandlers } from "@/tests/mocks/handlers/app";
+import { appHandlers } from "@/testing/mocks/handlers/app";
 
 // MSWのサーバーインスタンスを作成
 export const server = setupServer(...appHandlers);
